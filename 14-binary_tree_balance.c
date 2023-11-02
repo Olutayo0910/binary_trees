1c7c20d62f54b0a91e1de3d2a72595b4e4b0a61e
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-02 05:14:03 -0700

    task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -27,8 +27,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	{
 		size_t l, r;
 
-		l = tree->left ? 1 + binary_tree_height(tree->left) : 0;
-		r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
+		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((l > r) ? l : r);
 	}
 	return (0);
