3d07b46bd67a57f0687c9cea965062bb7b152541
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-11-02 00:39:11 -0700

    task 5 updated

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_is_root - checks if a node is a root
  * @node: A pointer to the node to check
- * Return 1 if node is a root, otherwise 0
+ * Return: 1 if node is a root, otherwise 0
  */
 int binary_tree_is_root(const binary_tree_t *node)
 {
